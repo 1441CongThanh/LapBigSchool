0ea1d3a7ab21a9debbf76b8e15f48f93e053f7ad
1441CongThanh <kyonest212@gmail.com>
2022-06-22 12:31:56 +0700

    Add project files.

---
+++ LapBigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LapBigSchool.Startup))]
+namespace LapBigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ LapBigSchool/ViewModels/CoursesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using LapBigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LapBigSchool.ViewModels
+{
+    public class CoursesViewModel
+    {
+        public IEnumerable<Course> UpcommingCourses { get; set; }
+        public bool ShowAction { get; set; }
+    }
+}
